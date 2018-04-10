6967edcbf0c3cb0acab1ca1f6f359749bc4a0d81
Levan <l.dvalidze@gmail.com>
2018-04-10 16:28:25 -0700

    bug fixed, no calculations yet

--- app.js
+++ app.js
@@ -1,36 +1,55 @@
 $(document).ready(function (){
     // Initialize Firebase
-var config = {
-    apiKey: "AIzaSyBXypnqNVunl8WZTKHXZ3gfpVOtSEq-xMg",
-    authDomain: "train-scheduler-9a94b.firebaseapp.com",
-    databaseURL: "https://train-scheduler-9a94b.firebaseio.com",
-    projectId: "train-scheduler-9a94b",
-    storageBucket: "",
-    messagingSenderId: "270428440514"
-  };
-  firebase.initializeApp(config);
+    var config = {
+        apiKey: "AIzaSyBXypnqNVunl8WZTKHXZ3gfpVOtSEq-xMg",
+        authDomain: "train-scheduler-9a94b.firebaseapp.com",
+        databaseURL: "https://train-scheduler-9a94b.firebaseio.com",
+        projectId: "train-scheduler-9a94b",
+        storageBucket: "",
+        messagingSenderId: "270428440514"
+    };
 
-  var dataRef = firebase.database();
+    firebase.initializeApp(config);
+
+    var dataRef = firebase.database();
 
     var name = "";
     var destination = "";
     var trainTime = 0;
-    var friquency = 0;
+    var frequency = 0;
 
-  $("#submit-train").on("click", function(event) {
- 
-    name = $("#train-name").val().trim();
-    destination = $("#train-destination").val().trim();
-    trainTime = $("#train-time").val().trim();
-    friquency = $("#friquency").val().trim();
+    $("#submit-train").on("click", function(event) {
 
-    // Code for the push
-    dataRef.ref().push({
-      name: name,
-      destination: destination,
-      trainTime: trainTime,
-      friquency: friquency,
-      dateAdded: firebase.database.ServerValue.TIMESTAMP
+        name = $("#train-name").val().trim();
+        destination = $("#train-destination").val().trim();
+        trainTime = $("#train-time").val().trim();
+        frequency = $("#train-frequency").val().trim();
+        dataRef.ref().push({
+            name: name,
+            destination: destination,
+            trainTime: trainTime,
+            frequency: frequency,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
     });
-  });
+
+    dataRef.ref().on("child_added", function(childSnapshot) {
+
+        // Log everything that's coming out of snapshot
+        console.log(childSnapshot.val().name);
+        console.log(childSnapshot.val().destination);
+        console.log(childSnapshot.val().trainTime);
+        console.log(childSnapshot.val().frequency);
+        console.log(childSnapshot.val().joinDate);
+  
+        // full list of items to the well
+        $("#nameDisplay").append("<div class='row'> " + childSnapshot.val().name + "</div>");
+        $("#destinationDisplay").append("<div class='row'> " + childSnapshot.val().destination + "</div>");
+        $("#frequencyDisplay").append("<div class='row'> " + childSnapshot.val().frequency + "</div>");
+        $("#nextDisplay").append("<div class='row'> " + childSnapshot.val().trainTime + "</div>");
+        $("#minutesAwayDisplay").append("<div class='row'> " + childSnapshot.val().trainTime + "</div>");
+        // Handle the errors
+        }, function(errorObject) {
+            console.log("Errors handled: " + errorObject.code);    
+    });
 })
